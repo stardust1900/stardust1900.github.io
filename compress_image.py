28a9abe8f4bd3f7d972c904288fe86015ced433a
stardust1900 <stardust1900@hotmail.com>
2024-06-20 03:08:11 +0000

    Deploying to docs from @ stardust1900/stardust1900.github.io@275f13ed5f08119d324efecb8225432b2d64b521 🚀

--- compress_image.py
+++ compress_image.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+from PIL import Image
 
 '''
 •  JPG 或 JPEG 格式的图片通常会比较小，因为它使用了有损压缩，适合存储颜色渐变平滑的自然场景或照片https://imagekit.io/blog/jpeg-vs-png-vs-gif-which-image-format-use/。
@@ -23,20 +24,25 @@ def traverse(dir):
         elif exp.match(fname.name):
             print("pic %s" % fname.name)
             print("path %s" % fname.path)
-            print("stat %s" % fname.stat().st_size)
+            print("stat %.2f kb" % (fname.stat().st_size / 1024))
+            compress(fname)
+            
 
-'''
-压缩图片
-'''
+
 def compress(pic):
     '''
     压缩图片
     '''
-    print("compress %s" % pic)
-    if jpgexp.match(pic):
-        os.system("jpegoptim -m80 --strip-all --all-progressive %s" % pic)
-    else:
-        os.system("optipng -o7 -strip all %s" % pic)
+    img = Image.open(pic.path)
+    print(img.size)
+    # 宽度超过850再压缩
+    if(img.size[0] > 850):
+        # 计算新的高度，保持纵横比
+        width_percent = (850 / float(img.size[0]))
+        new_height = int((float(img.size[1]) * float(width_percent)))
+        # 重新设置图片大小
+        img = img.resize((850, new_height), Image.LANCZOS)
+        img.save(pic.path, 'JPEG', quality=85)
     
 
 
@@ -46,6 +52,6 @@ if __name__ == '__main__':
     target = "./assets/images/"
     if(len(sys.argv)>=2):
         target = target+sys.argv[1]
-
-    print(target)
+    else:
+        exit(0)
     traverse(target)
